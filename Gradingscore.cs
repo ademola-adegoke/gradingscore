f475b96e2692a2f40ae592f9aa7b32666b77d415
Demola <demoladaniel30@gmail.com>
2023-05-03 05:05:03 -0700

    commit

---
+++ Gradingscore.cs
@@ -0,0 +1,18 @@
+*{
+
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+body{
+    height: 633px;
+}
+.Grading{
+    width: 28%;
+    padding: 30px;
+    height: 180px;
+    border-radius: 5px;
+    margin: 250px auto;
+    background-color: white;
+    box-shadow: rgb(60, 57, 57) 0px 15px 10px -2px;
+}
